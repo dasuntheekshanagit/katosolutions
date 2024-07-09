2bb8518d4d3d3297186bddbe8ce32412bfc80c08
Dasun Theekshana <dasun.theekshana.git@gmail.com>
2024-07-09 07:56:57 +0530

    add react routing and separate page for home

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -11,7 +11,7 @@ const NavBar = ({ page }: Props) => {
   return (
     <header id="header" className="header d-flex align-items-center fixed-top">
       <div className="container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
-        <a href="index.html" className="logo d-flex align-items-center">
+        <a href="/" className="logo d-flex align-items-center">
           <img src="assets/img/logo.webp" alt="" />
           <h1 className="sitename">KATO Solutions</h1> <span>.</span>
         </a>
---
+++ src/pages/Home.tsx
@@ -0,0 +1,23 @@
+import Footer from "../components/Footer";
+import GetStarted from "../components/GetStarted";
+import HeroSection from "../components/HeroSection";
+import NavBar from "../components/NavBar";
+import Services from "../components/Services";
+import StatsCounter from "../components/StatsCounter";
+import WhyUs from "../components/WhyUs";
+
+const Home = () => {
+  return (
+    <>
+      <NavBar page="home" />
+      <HeroSection />
+      <StatsCounter />
+      <WhyUs />
+      <Services />
+      <GetStarted />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
