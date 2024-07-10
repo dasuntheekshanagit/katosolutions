d92c9c4ed430fc7bdbad5ef61db5ab20ee3113dc
Dasun Theekshana <dasun.theekshana.git@gmail.com>
2024-07-10 09:50:38 +0530

    vite config to check bundle size

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    visualizer({
+      open: true, // Automatically open the visualization in your default browser
+      gzipSize: true,
+      brotliSize: true,
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          swiper: ['swiper'],
+          // Add more libraries here if needed
+        },
+      },
+    },
+    chunkSizeWarningLimit: 500, // Adjust the chunk size warning limit
+  },
 })
